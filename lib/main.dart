9784c6bbee90c7b7818b234e0d16dadc080933e0
perfectionist7 <akhandelwal2003@gmail.com>
2022-12-24 15:32:14 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -26,49 +26,67 @@ class LandingPage extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                       fontSize: 32),
                 )),
-            Container(
-              margin: new EdgeInsets.symmetric(horizontal: 100),
-              child: Image.asset(
-                'images/Vector3.png',
-                fit: BoxFit.fill,
-                width: 128.52,
-              ),
-            ),
             Stack(
               alignment: Alignment.center,
               children: [
                 Container(
                   child: Image.asset(
                     'images/Vector7.png',
-                    height: 300,
-                    width: 400,
                     fit: BoxFit.fill,
+                    height: 450,
+                    width: 405,
                   ),
                 ),
-                Container(
-                  child: Text(
-                    'Opportunity',
-                    style: TextStyle(color: Colors.white, fontSize: 40),
+                Positioned(
+                  top: 50,
+                  right: 80,
+                  child: Container(
+                    margin: new EdgeInsets.symmetric(horizontal: 100),
+                    child: Image.asset(
+                      'images/curvedimg.png',
+                      width: 128.52,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 280,
+                  child: Container(
+                    child: Text(
+                      'Opportunity',
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
                   ),
                 ),
               ],
             ),
             SizedBox(
-                child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Create A New Account',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    style:
-                        ElevatedButton.styleFrom(primary: Color(0xff5078E1)))),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                'Login',
-                style: TextStyle(color: Color(0xff5078E1)),
+              height: 50,
+            ),
+            SizedBox(
+              height: 42,
+              width: 324,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Create A New Account',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(primary: Color(0xff5078E1))),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 42,
+              width: 324,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Color(0xff5078E1)),
+                ),
+                style: ElevatedButton.styleFrom(primary: Colors.white),
               ),
-              style: ElevatedButton.styleFrom(primary: Colors.white),
             )
           ])),
     );
